194a79019bfe4ffcdc300394d0ead8705bc0005e
Alex Mark <alex.mark@mail.utoronto.ca>
2019-09-14 18:02:06 -0400

    fixed recipe page

--- web-app/src/components/recipeComponents/recipeList.jsx
+++ web-app/src/components/recipeComponents/recipeList.jsx
@@ -1,2 +1,31 @@
 import React from 'react';
+import {} from '../../assets/edamam_test.js';
 
+const createQuery = (ingredients) => {
+    let queryString = "";
+    ingredients.forEach((curr) => {
+        queryString += curr + ","
+    });
+
+    queryString = queryString.substring(queryString.length-1);
+}
+
+const RecipeList = (props) => {
+    let query = createQuery(props.ingredients);
+
+    //console.log(recipe);
+    return ( 
+        <div className="container custom-container">
+            <h1>Recipe List</h1>  
+                <div className="list">
+                    <ul>
+                        {/*.map((item) => {
+                            return (<li key={item.key}>{item.quantity}x {item.name}</li>);
+                        }) : ""*/}
+                    </ul>
+                </div>
+        </div>
+     );
+}
+ 
+export default RecipeList;
